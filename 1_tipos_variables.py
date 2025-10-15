c02948c580d53589b6105c6403b05efd32deea73
jonathan <jonathanballesterosg08@gmailcom>
2025-10-14 20:48:42 -0500

    first commit

---
+++ 1_tipos_variables.py
@@ -0,0 +1,18 @@
+int
+float
+str
+bool
+# ejemplos de dstos tipos int
+num = 3
+# ejemplos de datos tipo float
+decimal = 3.5
+# ejemplos de datos tipos string
+texto = "hola mundo"
+# ejemplos de datos tipos boolean
+isTrue = True
+isFalse = False
+
+print(type(num))
+print(type(decimal))
+print(type(texto))
+print(type(isFalse))
---
+++ 2_variables.py
@@ -0,0 +1,26 @@
+num1 = 3
+num2 = 5
+suma = num1 + num2
+print(suma)
+
+dec1 = 1.5
+dec2 = 12.7
+suma = dec1+dec2
+print(f'el resulatdo es {suma}')
+
+# comentar texto completo: control+k+c
+# descomentar texto completo: control+k+u
+
+numero1=int(input("ingrese un numero: "))
+numero2=int(input("ingrse otro numero: "))
+
+suma=numero1+numero2
+print(f"la suma es:{suma}")
+
+numero1=float(input("ingrese un numero: "))
+numero2=float(input("ingrse otro numero: "))
+
+resta=numero1-numero2
+print(f"la resta es:{resta}")
+
+
